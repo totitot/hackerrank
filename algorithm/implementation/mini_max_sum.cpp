1da2acf652c7210dce47e0edfab3782fed61bb3e
totitot <kenth.crisolo@gmail.com>
2016-12-19 18:31:18 +0800

    test ssh

--- algorithm/implementation/mini_max_sum.cpp
+++ algorithm/implementation/mini_max_sum.cpp
@@ -7,13 +7,16 @@ using namespace std;
 
 int main(){
 	vector<unsigned long long> vll;
+
 	for( int i = 0; i < 5; i++ ){
 		unsigned long long temp;
 		cin >> temp;
 		vll.push_back(temp);
 	}
+
 	sort( vll.begin(), vll.end() );
 	unsigned long long temp = accumulate( vll.begin(), vll.end(), 0ULL );
 	cout <<  temp - vll[4] << " " << temp - vll[0];
 	return 0;
+
 }
