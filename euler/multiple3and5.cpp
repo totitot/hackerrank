78f5e11612fb007913e4a88090e0fef2d77bcd1d
totitot <kenth.crisolo@gmail.com>
2017-01-04 14:16:35 +0800

    updated

--- euler/multiple3and5.cpp
+++ euler/multiple3and5.cpp
@@ -7,14 +7,18 @@ int main(){
 	cin >> t;
 	for( int a0 = 0; a0 < t; a0++ ){
 		int n; cin >> n;
-		int sum = 0;
-		for( int multiple3 = 3; multiple3 < n ; multiple3 += 3 ){
-			sum += multiple3;
-		}
-		for( int multiple5 = 5; multiple5 < n ; multiple5 += 5 ){
-			sum += multiple5;
-		}
-		cout << sum << endl;
-
+		uint64_t max3 = (n-1)/3;
+		uint64_t max5 = (n-1)/5;
+		uint64_t max15 = (n-1)/15;
+		uint64_t sum3 = 0;
+		uint64_t sum5 = 0;
+		uint64_t sum15 = 0;
+		sum3 = ( max3 * ( max3 + 1 ) ) / 2;
+		sum5 = ( max5 * ( max5 + 1 ) ) / 2;
+		sum15 = ( max15 * ( max15 + 1 ) ) / 2;
+		sum3 *= 3;
+		sum5 *= 5;
+		sum15 *= 15;
+		cout << sum3 + sum5 - sum15 << endl;
 	}
 }
